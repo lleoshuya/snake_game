5cd2478fe0150336f732f89bdd0ce7dd669ae9d3
Leonardo Henrique Rangon Paulino <lleoshuya@outlook.com>
2021-03-03 00:26:48 -0300

    Fim da Aula 05, incrementando a cobrinha quando ela atinje o alvo, bem como finalizando o jogo, caso a cobrinha atinja ela mesma.

--- js/js.js
+++ js/js.js
@@ -16,7 +16,7 @@ let food = {
 
 function criarBG() {
 
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "silver";
     context.fillRect(0, 0, 16 * box, 16 * box);
 
 };
@@ -24,14 +24,14 @@ function criarBG() {
 function criarCobrinha() {
 
     for (i = 0; i < snake.length; i++) {
-        context.fillStyle = "green";
+        context.fillStyle = "purple";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 
 };
 
 function drawFood() {
-    context.fillStyle = "red";
+    context.fillStyle = "pink";
     context.fillRect(food.x, food.y, box, box);
 };
 
@@ -56,6 +56,8 @@ function update(event) {
 
 function iniciarJogo() {
 
+
+
     if (snake[0].x > 15 * box && direction == "right") {
         snake[0].x = 0;
     }
@@ -68,6 +70,13 @@ function iniciarJogo() {
     if (snake[0].y < 0 && direction == "up") {
         snake[0].y = 16 * box;
     }
+
+    for (i = 1; i < snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo);
+            alert('Game Over!');
+        }
+    };
 
     criarBG();
     criarCobrinha();
@@ -89,7 +98,13 @@ function iniciarJogo() {
         snakeY += box;
     }
 
-    snake.pop();
+    if (snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+
 
     let newHead = {
         x: snakeX,
